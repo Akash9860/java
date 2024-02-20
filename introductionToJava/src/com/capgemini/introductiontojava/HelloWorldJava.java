a77a3f7f34948e025f1e1c499f28500f8e39a865
lenovo <lenovo@lenovo-PC-13>
2024-02-20 15:25:21 +0530

    commit

---
+++ introductionToJava/src/com/capgemini/introductiontojava/HelloWorldJava.java
@@ -0,0 +1,11 @@
+package com.capgemini.introductiontojava;
+// program todemonsytraye on printinga stATEMENT
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+		System.out.println("hey,there I AM WRITING HELLO WORLD PROGRAM" + "PROGRAM");
+		// TODO Auto-generated method stub
+
+	}
+
+}
